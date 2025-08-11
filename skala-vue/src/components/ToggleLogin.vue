861f69892f551db3c8f329246b6ef2a0161677ea
hyunji <hyunji4560@gmail.com>
2025-08-11 15:54:22 +0900

    refactor : v-if/else를 각각의 template으로 묶음

--- skala-vue/src/components/ToggleLogin.vue
+++ skala-vue/src/components/ToggleLogin.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
         <button @click="toggleLogin">Toggle Login</button>
-        <p v-if="isLoggedIn" id="login-true">Welcome back!</p>
-        <p v-else id="login-false">Please log in.</p>
+        <template v-if="isLoggedIn">
+            <!-- if-else가 같은 레벨이라 붙어있어서 한번에 불가능해서 template으로 묶어서 처리.-->
+            <p id="login-true">Welcome back!</p>
+            <a href="https://google.com" target="_blank">구글 이동</a>
+            <p>showshowshow</p>
+        </template>
+        <template v-else >
+            <p id="login-false">Please log in.</p>
+        </template>
     </div>
 </template>
 
